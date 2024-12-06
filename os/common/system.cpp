henriquecolini/laf

commit 3bea53125ccafce878d17fcf8d9d192ad69fafb4

Make listscreens example work even when backend=none
We've refactored the System hierarchy so we can make an instance of
SystemWin/OSX/X11 classes even when LAF_BACKEND=none. We've made
System::makeWin/OSX/X11() functions available as they might be enough
in certain cases where the whole Skia is not needed.

Some important changes in this patch:
* os::instance() is deprecated, replaced with os::System::instance()
* os::make_instance() is deprecated, replaced with os::System::make()
* os::System::instance() returns an os::SystemRef

os/common/system.cpp
@@ -1,0 +1,91 @@
+// LAF OS Library
+// Copyright (C) 2019-2024  Igara Studio S.A.
+// Copyright (C) 2012-2018  David Capello
+//
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "os/common/system.h"
+
+namespace os {
+
+// Weak reference to the unique system instance. This is destroyed by
+// the user (with the main SystemRef to the system).
+System* g_instance = nullptr;
+
+SystemRef System::instance()
+{
+  if (g_instance)
+    return AddRef(g_instance);
+  return nullptr;
+}
+
+SystemRef System::make()
+{
+  SystemRef ref;
+#if LAF_SKIA
+  ref = System::makeSkia();
+#endif
+#if LAF_WINDOWS
+  if (!ref)
+    ref = System::makeWin();
+#elif LAF_MACOS
+  if (!ref)
+    ref = System::makeOSX();
+#elif LAF_LINUX
+  if (!ref)
+    ref = System::makeX11();
+#endif
+  if (!ref)
+    ref = System::makeNone();
+
+  if (!g_instance)
+    g_instance = ref.get();
+
+  return ref;
+}
+
+void CommonSystem::errorMessage(const char* msg)
+{
+  fputs(msg, stderr);
+}
+
+// This must be called in the final class that derived from
+// CommonSystem, because clearing the list of events can generate
+// events on windows that will depend on the platform-specific
+// System.
+//
+// E.g. We've crash reports because WindowWin is receiving
+// WM_ACTIVATE messages when we destroy the events queue, and the
+// handler of that message is expecting the SystemWin instance (not
+// a CommonSystem instance). That's why we cannot call this from
+// ~CommonSystem() destructor and we have to call this from
+// ~SystemWin (or other platform-specific System implementations).
+void CommonSystem::destroyInstance()
+{
+  // destroyInstance() can be called multiple times by derived
+  // classes.
+  if (g_instance != this)
+    return;
+
+  // We have to reset the list of all events to clear all possible
+  // living WindowRef (so all window destructors are called at this
+  // point, when the os::System instance is still alive).
+  //
+  // TODO Maybe the event queue should be inside the System instance
+  //      (so when the system is deleted, the queue is
+  //      deleted). Anyway we should still clear all the events
+  //      before set_instance(nullptr), and we're not sure if this
+  //      is possible on macOS, as some events are queued before the
+  //      System instance is even created (see
+  //      EventQueue::instance() comment on laf/os/event_queue.h).
+  eventQueue()->clearEvents();
+
+  g_instance = nullptr;
+}
+
+} // namespace os

os/win/window.cpp
@@ -326,13 +326,13 @@ os::ScreenRef WindowWin::screen() const
     HMONITOR monitor = MonitorFromWindow(m_hwnd, MONITOR_DEFAULTTONEAREST);
     return os::make_ref<ScreenWin>(monitor);
   }
-  else
-    return os::instance()->mainScreen();
+  return os::System::instance()->mainScreen();
 }
 
 os::ColorSpaceRef WindowWin::colorSpace() const
 {
-  if (auto defaultCS = os::instance()->windowsColorSpace())
+  const os::SystemRef system = os::System::instance();
+  if (auto defaultCS = system->windowsColorSpace())
     return defaultCS;
 
   if (m_hwnd) {
@@ -346,7 +346,7 @@ os::ColorSpaceRef WindowWin::colorSpace() const
   }
   // sRGB by default
   if (!m_lastColorProfile)
-    m_lastColorProfile = os::instance()->makeColorSpace(gfx::ColorSpace::MakeSRGB());
+    m_lastColorProfile = system->makeColorSpace(gfx::ColorSpace::MakeSRGB());
   return m_lastColorProfile;
 }
 
@@ -2610,9 +2610,9 @@ void CALLBACK WindowWin::staticInteractionContextCallback(
 }
 
 // static
-SystemWin* WindowWin::system()
+Ref<SystemWin> WindowWin::system()
 {
-  return static_cast<SystemWin*>(os::instance());
+  return SystemWin::instance();
 }
 
 } // namespace os

os/none/system.cpp
@@ -1,0 +1,21 @@
+// LAF OS Library
+// Copyright (c) 2024  Igara Studio S.A.
+//
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "os/common/system.h"
+
+namespace os {
+
+class NoneSystem : public CommonSystem { };
+
+SystemRef System::makeNone() {
+  return make_ref<NoneSystem>();
+}
+
+} // namespace os

os/system.cpp
@@ -1,39 +1,0 @@
-// LAF OS Library
-// Copyright (C) 2020  Igara Studio S.A.
-// Copyright (C) 2016  David Capello
-//
-// This file is released under the terms of the MIT license.
-// Read LICENSE.txt for more information.
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include "base/debug.h"
-#include "os/ref.h"
-#include "os/system.h"
-
-namespace os {
-
-static System* g_system = nullptr;
-
-System* make_system_impl();   // Defined on each back-end
-
-SystemRef make_system()
-{
-  ASSERT(!g_system);
-  g_system = make_system_impl();
-  return SystemRef(g_system);
-}
-
-System* instance()
-{
-  return g_system;
-}
-
-void set_instance(System* system)
-{
-  g_system = system;
-}
-
-} // namespace os

os/win/system.cpp
@@ -315,4 +315,9 @@ void SystemWin::_setInternalMousePosition(const Event& ev)
   m_screenMousePos = ev.window()->pointToScreen(ev.position());
 }
 
+SystemRef System::makeWin()
+{
+  return make_ref<SystemWin>();
+}
+
 } // namespace os

os/x11/system.cpp
@@ -190,4 +190,9 @@ CursorRef SystemX11::makeCursor(const Surface* surface,
   return make_ref<CursorX11>(xcursor);
 }
 
+SystemRef System::makeX11()
+{
+  return make_ref<SystemX11>();
+}
+
 } // namespace os

os/x11/window.cpp
@@ -448,11 +448,16 @@ os::ScreenRef WindowX11::screen() const
 
 os::ColorSpaceRef WindowX11::colorSpace() const
 {
-  if (auto defaultCS = os::instance()->windowsColorSpace())
+  auto system = System::instance();
+  ASSERT(system);
+  if (!system)
+    return nullptr;
+
+  if (auto defaultCS = system->windowsColorSpace())
     return defaultCS;
 
   // TODO get the window color space
-  return os::instance()->makeColorSpace(gfx::ColorSpace::MakeSRGB());
+  return system->makeColorSpace(gfx::ColorSpace::MakeSRGB());
 }
 
 void WindowX11::setScale(const int scale)
@@ -763,7 +768,7 @@ void WindowX11::invalidateRegion(const gfx::Region& rgn)
 
 bool WindowX11::setCursor(NativeCursor nativeCursor)
 {
-  const CursorRef cursor = ((SystemX11*)os::instance())->getNativeCursor(nativeCursor);
+  const CursorRef cursor = SystemX11::instance()->getNativeCursor(nativeCursor);
   if (cursor)
     return setX11Cursor((::Cursor)cursor->nativeHandle());
   return false;

examples/CMakeLists.txt
@@ -15,6 +15,8 @@ function(laf_add_example name console="")
 endfunction()
 
 laf_add_example(base64 CONSOLE)
+laf_add_example(listfonts CONSOLE)
+laf_add_example(listscreens CONSOLE)
 laf_add_example(show_platform CONSOLE)
 if(LAF_BACKEND STREQUAL "skia")
   laf_add_example(allevents GUI)
@@ -22,8 +24,6 @@ if(LAF_BACKEND STREQUAL "skia")
   laf_add_example(custom_window GUI)
   laf_add_example(floating_window GUI)
   laf_add_example(helloworld GUI)
-  laf_add_example(listfonts CONSOLE)
-  laf_add_example(listscreens CONSOLE)
   laf_add_example(multiple_windows GUI)
   laf_add_example(panviewport GUI)
   laf_add_example(shader GUI)

commit d3c48fcc3882969467878682abecac6ab38bb4ea

[win] Don't add new ref to a System being destroyed (fix aseprite/aseprite#4811)
Regression introduced in 3bea53125ccafce878d17fcf8d9d192ad69fafb4,
when os::instance() was refactored to os::System::instance() to return
a SystemRef instead of a System*, and ~WindowWin() destructor asks for
the current system() (and WindowWin::system() is trying to create a
new System reference, but the System is being destroyed as it was
completely unreferred, i.e. ref=0.)

os/common/system.cpp
@@ -25,14 +25,22 @@
   #include "clip/clip.h"
 #endif
 
+#include "base/debug.h"
+
 namespace os {
 
 // Weak reference to the unique system instance. This is destroyed by
 // the user (with the main SystemRef to the system).
 System* g_instance = nullptr;
 
+// Flag to know if the intance is already being destroyed, so we
+// cannot add a ref to it, i.e. calling System::instance() is illegal
+// if this flag is true.
+static bool g_is_being_destroyed = false;
+
 SystemRef System::instance()
 {
+  ASSERT(!g_is_being_destroyed);
   if (g_instance)
     return AddRef(g_instance);
   return nullptr;
@@ -40,6 +48,8 @@ SystemRef System::instance()
 
 SystemRef System::make()
 {
+  ASSERT(!g_instance);
+
   SystemRef ref;
 #if LAF_SKIA
   ref = System::makeSkia();
@@ -72,7 +82,7 @@ CommonSystem::~CommonSystem()
 {
   // destroyInstance() can be called multiple times by derived
   // classes.
-  if (instance() == this)
+  if (g_instance == this)
     destroyInstance();
 }
 
@@ -208,8 +218,12 @@ void CommonSystem::destroyInstance()
 {
   // destroyInstance() can be called multiple times by derived
   // classes.
-  if (g_instance != this)
+  if (g_instance != this) {
+    ASSERT(g_is_being_destroyed);
     return;
+  }
+
+  g_is_being_destroyed = true;
 
   // We have to reset the list of all events to clear all possible
   // living WindowRef (so all window destructors are called at this

os/win/window.cpp
@@ -66,6 +66,9 @@
 
 namespace os {
 
+// Extern raw reference to current system instance.
+extern System* g_instance;
+
 // Converts an os::Hit to a Win32 hit test value
 static int hit2hittest[] = {
   HTNOWHERE,                    // os::Hit::None
@@ -135,7 +138,7 @@ static BOOL CALLBACK log_monitor_info(HMONITOR monitor,
 
 std::wstring get_wnd_class_name()
 {
-  if (auto sys = instance()) {
+  if (auto sys = os::System::instance()) {
     if (!sys->appName().empty())
       return base::from_utf8(sys->appName());
   }
@@ -322,12 +325,17 @@ WindowWin::WindowWin(const WindowSpec& spec)
 
 WindowWin::~WindowWin()
 {
-  auto sys = system();
+  // We cannot use os::System::instance() here because that would add
+  // a new reference to a possible dying System pointer, e.g. when we
+  // come from ~System because the last Ref::unref() was called and
+  // this window is being destroyed because its last reference was in
+  // a os::Event of the os::EventQueue.
+  SystemWin* sys = (SystemWin*)g_instance;
 
   // If this assert fails it's highly probable that an os::WindowRef
   // was kept alive in some kind of memory leak (or just inside an
   // os::Event in the os::EventQueue). Also this can happen when
-  // declaring a os::WindowRef before calling os::make_system(),
+  // declaring a os::WindowRef before calling os::System::system(),
   // because of deletion order when destructors got called.
   ASSERT(sys);
 